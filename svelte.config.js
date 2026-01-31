451acccb52de90d48e3467f67b0b8db63ab32a3b
Sakhawat Hossain <sakhawathossain2409@gmail.com>
2026-01-31 20:06:07 +0600

    Hardcode base path for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			strict: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/Portfolio' : ''
+			base: '/Portfolio'
 		},
 		prerender: {
 			handleMissingId: 'ignore'
